c4f8b3ca9c3ff3568f833205da9486bf460bf4ba
dewe biag <dewebiag@gmail.com>
2025-03-13 22:47:00 -0700

    firstperson

--- src/Camera.jsx
+++ src/Camera.jsx
@@ -1,17 +1,41 @@
 import { useState, useEffect } from "react"; 
+import { useThree } from "@react-three/fiber";
 
-import { useFrame } from '@react-three/fiber';
 import { PerspectiveCamera } from '@react-three/drei';
+import { PointerLockControls } from '@react-three/drei';
 
 
 // pass camera x pos as prop in 
-const Camera = ({ camPosX }) => {
+const Camera = () => {
+  const { camera, gl } = useThree(); 
+
+  useEffect(() => {
+    const handleResize = () => {
+      console.log("hello");
+      console.log(window.innerWidth);
+      const width = window.innerWidth; 
+      const height = window.innerHeight;
+
+      camera.aspect = width / height;
+      camera.updateProjectionMatrix();
+
+      gl.setSize(width, height);
+      gl.setPixelRatio(window.devicePixelRatio); 
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // call on run so aspect is initialized
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [camera, gl]);
 
   return (
     <PerspectiveCamera 
       makeDefault
-      position={[camPosX, 35, 150]} 
-      fov={35} />
+      position={[0, 5, 0]} 
+      fov={35} >
+      <PointerLockControls />
+    </PerspectiveCamera>
   );
 };
 
--- src/ThreeScene.jsx
+++ src/ThreeScene.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useMemo, useEffect } from 'react';
 
 import { Canvas } from '@react-three/fiber';
 
+
 import Player from "./Player";
 import Ground from "./Ground";
 import Camera from "./Camera";
@@ -9,19 +10,13 @@ import Camera from "./Camera";
 import "./styles.css";
 
 const ThreeScene = () => {
-  let [camPosX, setCamPosX] = useState(); 
-
-  const getCamPosX = (newX) => {
-    setCamPosX(newX); 
-  }
 
   return (
-    <Canvas style={{ display: 'block', width: '1904px', height: '550px' }}>
-      <Camera camPosX={camPosX} /> 
+    <Canvas>
+      <Camera /> 
       <ambientLight intensity={0.5} />
       <directionalLight position={[10, 10, 5]} intensity={1} />
       <Ground /> 
-      <Player setCamPosX={setCamPosX}/> {/* pass callback function in. this is the function used */}
     </Canvas>
   );
 };
