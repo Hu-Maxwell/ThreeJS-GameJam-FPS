7c4d748e19d72b06645fd04ada502b3db59066d5
Vchen7629 <chenvincent7629@gmail.com>
2025-03-13 22:53:51 -0700

    added ground textures

--- src/Ground.jsx
+++ src/Ground.jsx
@@ -2,10 +2,23 @@ import React, { useRef, useMemo, useEffect } from 'react';
 import * as THREE from 'three';
 
 const Ground = () => {
+  const texture = useMemo(() => new THREE.TextureLoader().load('/cobblestone_1.png'), []);
+  
+  useEffect(() => {
+    if (texture) {
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      texture.repeat.set(10, 20); 
+    }
+  }, [texture]);
+
   return (
     <mesh rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[50, 200]} /> {/* args: width height (actually height width but it's rotated)*/}
-      <meshBasicMaterial color={0x228B22} side={THREE.DoubleSide} /> 
+      <planeGeometry args={[100, 200]} /> 
+      <meshBasicMaterial 
+        attach="material" 
+        map={texture} 
+        side={THREE.DoubleSide}
+      />
     </mesh>
   );
 };
